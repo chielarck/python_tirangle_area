59675bc9d607bd1a8e6f3454c2fb82c5bb59fef2
chielarck <egigusniawanpradana@gmail.com>
2016-02-18 20:19:45 +0700

    versi 1.0

---
+++ hitung_segitiga.py
@@ -0,0 +1,30 @@
+class Mtk:
+   def hitung_pakai_class(self, height, base):
+      return height * base / 2
+segitiga1 = Mtk()
+segitiga2 = Mtk()
+segitiga3 = Mtk()
+result1 = segitiga1.hitung_pakai_class(10,2)
+result2 = segitiga2.hitung_pakai_class(30,5)
+result3 = segitiga3.hitung_pakai_class(15,2)
+print ("Segitiga 1 = ", result1)
+print ("Segitiga 2 = ", result2)
+print ("Segitiga 3 = ", result3)
+
+tinggi = 24
+alas = 10
+luas = alas * tinggi / 2
+
+print ("Luas adalah ", luas)
+
+def hitung_segitiga(alas,tinggi):
+    return alas * tinggi / 2
+
+hasil1 = hitung_segitiga(2,10)
+hasil2 = hitung_segitiga(2,15)
+hasil3 = hitung_segitiga(30,2)
+
+print ("hasil 1 adalah : ",hasil1)
+print ("hasil 2 adalah : ",hasil2)
+print ("hasil 3 adalah : ",hasil3)
+
